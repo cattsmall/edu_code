dfa3c4392d32c4ad77f01dfbb3ed5a66fd32955b
Catt Small <cattsmall@gmail.com>
2013-04-24 23:54:59 -0400

    Setting up the second page

---
+++ inc/forms/search.php
@@ -0,0 +1,8 @@
+<input type="text">
+<div class="dropdown">
+	<a class="btn dropdown-toggle" data-toggle="dropdown">Content types</a>
+</div>
+<div class="dropdown">
+	<a class="btn dropdown-toggle" data-toggle="dropdown">Difficulty</a>
+</div>
+<input type="submit" value="search" class="ss-icon ss-pika">
